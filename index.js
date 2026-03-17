e1960a68b2482f6c5619321fbbf0895f710ecd70
Code-God-2010 <linlagode@gmail.com>
2026-03-17 21:41:27 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+let started = false;
+
+function startVideo() {
+  if (started) return;
+  started = true;
+
   const iframe = document.getElementById("yt");
   iframe.src += "&autoplay=1";
-  iframe.src -= "&mute=1";
-  iframe.src += "&mute=0";
-}, {once: true});
+}
+
+["mousemove", "click", "touchstart"].forEach(event => {
+  document.addEventListener(event, startVideo);
+});
+
+function startCountdown(callback) {
+    const targetDate = new Date("2027-07-29T00:00:00");
+
+    setInterval(() => {
+        const now = new Date();
+        const diff = targetDate - now;
+
+        if (diff <= 0) {
+            callback(0, 0, 0);
+            return;
+        }
+        const totalSeconds = Math.floor(diff / 1000);
+
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
+
+        callback(hours, minutes, seconds);
+
+    }, 1000);
+}
+
+startCountdown((h, m, s) => {
+    document.getElementById("timer").textContent =
+        `${h} Stunden, ${m} Minuten, ${s} Sekunden`;
+});
