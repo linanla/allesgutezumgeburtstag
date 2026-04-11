3291d81f038e8f36ecaa52361cdf1e7c8c83cba4
Code-God-2010 <linlagode@gmail.com>
2026-04-11 23:11:54 +0200

    sdfgsdfgdfggfd

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 let started = false;
 
 function startVideo() {
-  if (started) return;
-  started = true;
+	if (started) return;
+	started = true;
 
-  const iframe = document.getElementById("yt");
-  iframe.src += "&autoplay=1";
+	const iframe = document.getElementById("yt");
+	iframe.src += "&autoplay=1";
 }
 
-["mousemove", "click", "touchstart"].forEach(event => {
-  document.addEventListener(event, startVideo);
+["mousemove", "click", "touchstart"].forEach((event) => {
+	document.addEventListener(event, startVideo);
 });
 
 function startCountdown(callback) {
-    const targetDate = new Date("2027-07-29T00:00:00");
+	const targetDate = new Date("2027-07-29T00:00:00");
 
-    setInterval(() => {
-        const now = new Date();
-        const diff = targetDate - now;
+	setInterval(() => {
+		const now = new Date();
+		const diff = targetDate - now;
 
-        if (diff <= 0) {
-            callback(0, 0, 0);
-            return;
-        }
-        const totalSeconds = Math.floor(diff / 1000);
+		if (diff <= 0) {
+			callback(0, 0, 0);
+			return;
+		}
+		const totalSeconds = Math.floor(diff / 1000);
 
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
+		const hours = Math.floor(totalSeconds / 3600);
+		const minutes = Math.floor((totalSeconds % 3600) / 60);
+		const seconds = totalSeconds % 60;
 
-        callback(hours, minutes, seconds);
-
-    }, 1000);
+		callback(hours, minutes, seconds);
+	}, 1000);
 }
 
 startCountdown((h, m, s) => {
-    document.getElementById("timer").textContent =
-        `${h} Stunden, ${m} Minuten, ${s} Sekunden`;
-    document.getElementById("spanTimer").textContent =
-        `${h} Stunden, ${m} Minuten, ${s} Sekunden`;
+	document.getElementById("timer").textContent =
+		`${h} Stunden, ${m} Minuten, ${s} Sekunden`;
+	document.getElementById("spanTimer").textContent =
+		`${h} Stunden, ${m} Minuten, ${s} Sekunden`;
 });
+
+let width = 0;
+setInterval(() => {
+	width += (Math.floor(Math.random() * (7 - -3) ) -3)/10;
+	document.getElementById("bar").style.width = width + "%";
+}, 90);
